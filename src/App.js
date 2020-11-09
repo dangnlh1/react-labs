ca27e2531bb040562e9039c1d9e51f08100d0f87
nlhdang <dangnlhse141070@fpr.edu.vn>
2020-11-09 21:27:24 +0700

    Add new title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React <code>src/App.js</code> vui
+          React <code>src/App.js</code> vui qua ba
         </p>
         <a
           className="App-link"
